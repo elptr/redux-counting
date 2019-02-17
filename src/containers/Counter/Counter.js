3ad1779d92382dd4a1ef60e36ad1cb8dd62908d7
administrator <lena.petrova.v@gmail.com>
2019-02-17 15:55:34 +0000

    add actions to reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -26,14 +26,13 @@ class Counter extends Component {
     }
 
     render () {
-        console.log(this.props.ctr + 2);
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -44,4 +43,12 @@ const mapStateToProps = state =>{
         ctr: state.counter
     }
 }
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type:'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type:'DECREMENT'}),
+        onAddCounter: () => dispatch({type:'ADD', val:5}),
+        onSubtractCounter: () => dispatch({type:'SUBTRACT', val:5})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,7 +4,31 @@ const initialState = {
 
 
 const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                counter:state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter:state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter:state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                counter:state.counter - action.val
+            }
+
+    }
+
     return state
 }
+
+
+
 
 export default reducer;
