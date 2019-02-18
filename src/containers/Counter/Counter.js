e6edf8a00d6aa23dc4fac01dbd58ad17475dedab
administrator <lena.petrova.v@gmail.com>
2019-02-18 09:31:23 +0000

    updating state and arr immutable

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -33,6 +33,17 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map((storedResult =>
+                        <li key = {storedResult.id}
+                            // onClick={this.props.onDeleteResult}> passing id to it
+                            onClick={() => this.props.onDeleteResult(storedResult.id)}>
+                            {storedResult.value}
+                        </li>
+                    ))}
+
+                </ul>
             </div>
         );
     }
@@ -40,7 +51,8 @@ class Counter extends Component {
 
 const mapStateToProps = state =>{
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 }
 const mapDispatchToProps = dispatch => {
@@ -48,7 +60,10 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type:'INCREMENT'}),
         onDecrementCounter: () => dispatch({type:'DECREMENT'}),
         onAddCounter: () => dispatch({type:'ADD', val:5}),
-        onSubtractCounter: () => dispatch({type:'SUBTRACT', val:5})
+        onSubtractCounter: () => dispatch({type:'SUBTRACT', val:5}),
+        onStoreResult: ()=>dispatch({type:'STORE_RESULT'}),
+        onDeleteResult: (id)=>dispatch({type:'DELETE_RESULT', resultElId:id}),
+        //id any name, and to receive it you need to pass it in <li onClick={this.props.onDeleteResult}>
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    counter:0
+    counter:0,
+    results:[]
 }
 
 
@@ -7,20 +8,54 @@ const reducer = (state = initialState, action) => {
 
     switch(action.type){
         case 'INCREMENT':
-            return {
-                counter:state.counter + 1
-            }
+            const newState = Object.assign({}, state); // not deep copy, rusults Arr the same Arr
+            newState.counter = state.counter + 1;
+            return newState;
         case 'DECREMENT':
             return {
+                ...state, //not deep copy too
                 counter:state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter:state.counter + action.val
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter:state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value:state.counter}),
+                //not push, because push change original arr
+
+            }
+        case 'DELETE_RESULT':
+            // //First way of copy array immutable
+            // const id=2; //index  //state.result.splice(id, 1); //mutates original array
+            // const newArray = [...state.result]
+            // //create copy of array, if elements of arrs
+            // //are objects, that need to copy them too, otherwise they are the same objects,
+            // //it is enough for removing element, because you are not changing original array
+            // newArray.splice(id, 1);
+
+            // //Second method is use filter()
+            // const id=2;
+            // const updatedArray = state.results.filter(result => true);
+            // //create of copy of array - long  for [...state.results,
+            // // but not always want to return true, only for elements that should be in new array
+            // //if we want to delete it we should return true for all elements that does not have certain index or id
+            // const updatedArray = state.results.filter((result, index) => index !== id);
+            // // we have id in {id: new Date(), value:state.counter}, not index, so we don't need index,
+            // // only check for id, that comes from payload
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+
             }
 
     }
